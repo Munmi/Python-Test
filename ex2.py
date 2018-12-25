ec62994c6eb078be83d5e7037d4d758d7a5bc638
root <root@ip-172-31-20-248.ap-south-1.compute.internal>
2018-12-25 12:11:46 +0000

    Exercises completed for 25th Dec

---
+++ ex2.py
@@ -0,0 +1,12 @@
+print " I will now count my chickens"
+print "Hens" , 25 + 30 /5
+print "Roosters" , 100 - 25 * 3 % 4
+print " Now I will count the eggs:"
+print 3 +2 + 1 -5 + 4 % 2 
+print "What is 5 + 2 - 7 ?" , 5 + 2 - 8 
+print 7 / 4
+print 7.0 / 4.0
+print 22 / 7
+print 22.0 / 7
+print 22.0 / 7.0
+print 22 / 7.0
---
+++ ex4.py
@@ -0,0 +1,14 @@
+MY_NAME = 'Munmi Upadhyay'
+MY_AGE = 28
+MY_HEIGHT = 5.4
+MY_WEIGHT = 50
+MY_EYES = 'Brown'
+MY_HAIR = 'Black'
+
+print "Let's talk about %s." % MY_NAME
+print "She's %d inches tall." % MY_HEIGHT
+print "She's %d pounds heavy." % MY_WEIGHT
+print "Actually that's not too heavy." 
+print "She's got %s eyes and %s hair." % (MY_EYES, MY_HAIR)
+
+print "If I add %d, %d, and %d I get %d." % (MY_AGE, MY_HEIGHT, MY_WEIGHT, MY_AGE + MY_HEIGHT + MY_WEIGHT)
